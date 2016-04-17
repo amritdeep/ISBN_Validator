b16dcccaa3ba2d826ff3aa01965f050f48e6c296
Amrit Deep Dhungana <amritdeepdhungana@hotmail.com>
2016-04-17 13:47:29 -0500

    create method check_isbn to check the isbn number

--- lib/isbnvalidator.rb
+++ lib/isbnvalidator.rb
@@ -7,15 +7,25 @@ class IsbnValidator
 		@isbn_num = isbn_num
 	end
 
-	def convert_into_array
-		@isbn_arry = @isbn_num.gsub(/-/,'').split('').map(&:to_i)
+	def check_isbn
+		convert_into_array(@isbn_num)
+		if @isbn_arry.count == 10
+			calculate_isbn
+		else
+			invalid
+		end
+	end
+
+	private
+
+	def convert_into_array(number)
+		@isbn_arry = number.gsub(/-/,'').split('').map(&:to_i)
 	end
 
 	def invalid
 		puts "Invalid ISBN Number"
 		return false
 	end
-
 
 	def calculate_isbn
 		result = ((@isbn_arry[0]*10) + (@isbn_arry[1]*9) + (@isbn_arry[2]*8) + (@isbn_arry[3]*7) + (@isbn_arry[4]*6) + (@isbn_arry[5]*5) + (@isbn_arry[6]*4) + (@isbn_arry[7]*3) + (@isbn_arry[8]*2) + (@isbn_arry[9]*1))/11
--- validator.rb
+++ validator.rb
@@ -4,6 +4,7 @@ require_relative 'lib/isbnvalidator'
 
 input = ARGV[0]
 isbn_num = IsbnValidator.new(input)
-isbn_num.convert_into_array
-isbn_num.calculate_isbn
-isbn_num.double_check_isbn
+isbn_num.check_isbn
+# isbn_num.convert_into_array
+# isbn_num.calculate_isbn
+# isbn_num.double_check_isbn
