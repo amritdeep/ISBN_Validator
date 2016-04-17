ba8451efc135aef67c0ba8ec869d24f91efd81d7
Amrit Deep Dhungana <amritdeepdhungana@hotmail.com>
2016-04-17 13:43:07 -0500

    create invalid method and test it

--- lib/isbnvalidator.rb
+++ lib/isbnvalidator.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
 
+require 'pry'
+
 class IsbnValidator
 	def initialize(isbn_num)
 		@isbn_num = isbn_num
@@ -8,6 +10,12 @@ class IsbnValidator
 	def convert_into_array
 		@isbn_arry = @isbn_num.gsub(/-/,'').split('').map(&:to_i)
 	end
+
+	def invalid
+		puts "Invalid ISBN Number"
+		return false
+	end
+
 
 	def calculate_isbn
 		result = ((@isbn_arry[0]*10) + (@isbn_arry[1]*9) + (@isbn_arry[2]*8) + (@isbn_arry[3]*7) + (@isbn_arry[4]*6) + (@isbn_arry[5]*5) + (@isbn_arry[6]*4) + (@isbn_arry[7]*3) + (@isbn_arry[8]*2) + (@isbn_arry[9]*1))/11
--- test/test_isbn_validator.rb
+++ test/test_isbn_validator.rb
@@ -12,6 +12,12 @@ class TestIsbnValidator < Test::Unit::TestCase
 		assert_equal([0, 3, 0, 6, 4, 0, 6, 1, 5, 2], isbn.convert_into_array)
 	end
 
+	def test_invalid
+		isbn = IsbnValidator.new('0-306-40615-223')
+		isbn.convert_into_array	
+		assert_equal(false, isbn.invalid)	
+	end
+
 	def test_calculate_isbn
 		isbn = IsbnValidator.new('0-306-40615-2')
 		isbn.convert_into_array
