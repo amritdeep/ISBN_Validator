83f54ea58a64eb7d4532893cc91944dcf38835ce
Amrit Deep Dhungana <amritdeepdhungana@hotmail.com>
2016-04-17 13:49:37 -0500

    test for check_isbn method

--- test/test_isbn_validator.rb
+++ test/test_isbn_validator.rb
@@ -7,27 +7,32 @@ require "test/unit"
 
 class TestIsbnValidator < Test::Unit::TestCase
 
-	def test_convert_to_arry
+	def test_check_isbn
 		isbn = IsbnValidator.new('0-306-40615-2')
-		assert_equal([0, 3, 0, 6, 4, 0, 6, 1, 5, 2], isbn.convert_into_array)
+		assert_equal(true, isbn.check_isbn)
 	end
 
-	def test_invalid
-		isbn = IsbnValidator.new('0-306-40615-223')
-		isbn.convert_into_array	
-		assert_equal(false, isbn.invalid)	
-	end
+	# def test_convert_to_arry
+	# 	isbn = IsbnValidator.new('0-306-40615-2')
+	# 	assert_equal([0, 3, 0, 6, 4, 0, 6, 1, 5, 2], isbn.convert_into_array)
+	# end
 
-	def test_calculate_isbn
-		isbn = IsbnValidator.new('0-306-40615-2')
-		isbn.convert_into_array
-		assert_equal(true, isbn.calculate_isbn)
-	end
+	# def test_invalid
+	# 	isbn = IsbnValidator.new('0-306-40615-223')
+	# 	isbn.convert_into_array	
+	# 	assert_equal(false, isbn.invalid)	
+	# end
 
-	def test_double_check_isbn
-		isbn = IsbnValidator.new('0-306-40615-2')
-		isbn.convert_into_array
-		assert_equal(true, isbn.double_check_isbn)
-	end
+	# def test_calculate_isbn
+	# 	isbn = IsbnValidator.new('0-306-40615-2')
+	# 	isbn.convert_into_array
+	# 	assert_equal(true, isbn.calculate_isbn)
+	# end
+
+	# def test_double_check_isbn
+	# 	isbn = IsbnValidator.new('0-306-40615-2')
+	# 	isbn.convert_into_array
+	# 	assert_equal(true, isbn.double_check_isbn)
+	# end
 
 end
