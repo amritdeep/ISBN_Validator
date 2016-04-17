7eb5f7239c4b2d07b86522c700420a36f2236e88
Amrit Deep Dhungana <amritdeepdhungana@hotmail.com>
2016-04-17 13:50:47 -0500

    test for invalid check isbn nmethod

--- test/test_isbn_validator.rb
+++ test/test_isbn_validator.rb
@@ -7,9 +7,14 @@ require "test/unit"
 
 class TestIsbnValidator < Test::Unit::TestCase
 
-	def test_check_isbn
+	def test_check_isbn_true
 		isbn = IsbnValidator.new('0-306-40615-2')
 		assert_equal(true, isbn.check_isbn)
+	end
+
+	def test_check_isbn_false
+		isbn = IsbnValidator.new('0-306-40615-22')
+		assert_equal(false, isbn.check_isbn)
 	end
 
 	# def test_convert_to_arry
